eee340a40bfe2fda032ddde847afba7b435c0783
Johan Grahn <high@Mac.local>
2017-06-04 21:39:59 +0200

    Added a hello message

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
+		
+			// Here we are going to add the awesome stuff
+
+			System.out.println("Pernilla is best");
+	}
+
 }
